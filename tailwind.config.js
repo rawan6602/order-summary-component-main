5422e7321f0c8bf910b2a7d3d54ffe2083ccf336
rawan6602 <109319561+rawan6602@users.noreply.github.com>
2023-07-10 07:56:12 +0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ export default {
     "./*css"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+      'dark-blue':'hsl(223, 47%, 23%)',
+      'primary':'hsl(245, 75%, 52%)',
+      'light-blue':' hsl(224, 23%, 55%)'
+      }
+    },
+    fontFamily:{
+      'body':['"Red Hat Display", sans-serif']
+    }
+
+
   },
   plugins: [],
 }
