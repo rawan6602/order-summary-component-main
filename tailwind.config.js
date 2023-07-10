af0fa4ab4e3136393df459f35daa4a6124e7e13d
rawan6602 <109319561+rawan6602@users.noreply.github.com>
2023-07-10 08:44:25 +0300

    fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,13 @@ export default {
       colors:{
       'dark-blue':'hsl(223, 47%, 23%)',
       'primary':'hsl(245, 75%, 52%)',
-      'light-blue':' hsl(224, 23%, 55%)'
+      'light-blue':' hsl(224, 23%, 55%)',
+      'hover':'#766cf1',
+      'pale-blue':'hsl(225, 100%, 98%) ',
+      'background':'hsl(225, 100%, 94%)'
+      },
+      backgroundImage: {
+        'background-img': "url('/images/pattern-background-desktop.svg')",
       }
     },
     fontFamily:{
